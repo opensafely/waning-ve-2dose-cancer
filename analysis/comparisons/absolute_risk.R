2bb0521b7df5f2ba4f3b13af2222e68a1f3f83ed
Elsie Horne <31345323+elsie-h@users.noreply.github.com>
2022-07-07 08:36:22 +0100

    annotate absolute_risk

--- analysis/comparisons/absolute_risk.R
+++ analysis/comparisons/absolute_risk.R
@@ -1,25 +1,32 @@
 ###############################################################################
+# calculate the standardised absolute risk (AR) in the cancer and noncancer cohorts
+
+# three options:
+
+# 1. age- and sex-standardised within cohort x vaccine arm
+# 2. vaccine arm-, age- and sex-standardised within cohort
+# 3. age- and sex-standardised within cohort
+
+###############################################################################
 library(tidyverse)
 library(glue)
 
 ###############################################################################
-# standardised absolute risk (AR) in cancer and noncancer cohorts
+# setup
 
-###############################################################################
 # read outcomes
 outcomes <- readr::read_rds(
   here::here("analysis", "lib", "outcomes.rds"))
 
-################################################################################
 # redaction functions
 source(here::here("analysis", "functions", "redaction_functions.R"))
 
-################################################################################
 # create output directory
 fs::dir_create(here::here("output", "tte", "tables"))
 
 ###############################################################################
-# read tte data
+# read data
+# time to event data split into 6 comparison periods
 data_tte <- bind_rows(
   lapply(
     outcomes,
@@ -31,14 +38,14 @@ data_tte <- bind_rows(
   )
 ) %>%
   mutate(
-    subgroup = if_else(
+    cohort = if_else(
       cancer_subgroup %in% "noncancer", 
       cancer_subgroup, 
       "cancer"
       )
     ) %>%
   mutate(across(arm, ~if_else(.x %in% "unvax", as.character(.x), "vax"))) %>%
-  select(patient_id, subgroup, arm, k, outcome, status)
+  select(patient_id, cohort, arm, k, outcome, status)
 
 # define age bands
 age_bands <- c(-Inf, seq(25,90,5), Inf)
@@ -55,11 +62,11 @@ data_all <- readr::read_rds(
   mutate(across(age, ~cut(.x, breaks = age_bands, labels = age_band_labs)))
 
 ###############################################################################
-### age- and sex-standardised AR within vaccine arms
+### 1. age- and sex-standardised AR within cohort x vaccine arm
 # AR grouped by arm, age, sex
 data_counts_arm_group <- data_tte %>%
   left_join(data_all, by = "patient_id") %>% 
-  group_by(subgroup, arm, outcome, k, age, sex) %>%
+  group_by(cohort, arm, outcome, k, age, sex) %>%
   summarise(
     n = n(),
     events = sum(status),
@@ -71,7 +78,7 @@ data_counts_arm_group <- data_tte %>%
 # within each outcome, arm and comparison period, 
 # get proportion of general population in each sex:age_band
 weights_arm_group <- data_counts_arm_group %>%
-  filter(subgroup == "noncancer") %>%
+  filter(cohort == "noncancer") %>%
   group_by(outcome, arm, k) %>%
   mutate(prop = n/sum(n)) %>%
   # mutate(total = sum(n)) %>%
@@ -84,10 +91,14 @@ data_ar_arm_group <- data_counts_arm_group %>%
   left_join(
     weights_arm_group, by = c("arm", "outcome", "k", "age", "sex")
   ) %>%
-  group_by(subgroup, arm, outcome, k) %>%
+  group_by(cohort, arm, outcome, k) %>%
   summarise(
     events = sum(events),
+    # calculate crude AR 
+    # (this is just to make sure that it is the same as the weighted average in 
+    # the noncancer cohort)
     ar_crude = sum(events)/sum(n),
+    # calculate the age- and sex-standardised AR
     ar_weighted = weighted.mean(x = ar, w = weight),
     .groups = "keep"
   ) %>%
@@ -96,7 +107,7 @@ data_ar_arm_group <- data_counts_arm_group %>%
   # if redact one arm, redact other too to avoid potential for back calculation
   # from previously released results
   mutate(redact = events <= 5) %>%
-  group_by(subgroup, outcome, k) %>%
+  group_by(cohort, outcome, k) %>%
   mutate(redact = as.logical(max(redact))) %>%
   ungroup() %>%
   mutate(across(c("events", "ar_crude", "ar_weighted"), 
@@ -108,11 +119,11 @@ readr::write_csv(
   here::here("output", "tte", "tables", "ar_arm_group.csv")
   )
 
-### arm-, age- and sex-standardised AR
+### 2. arm-, age- and sex-standardised AR within cohort
 # within each outcome and comparison period, 
 # get proportion of general population in each arm:sex:age_band
 weights_arm_weight <- data_counts_arm_group %>%
-  filter(subgroup == "noncancer") %>%
+  filter(cohort == "noncancer") %>%
   group_by(outcome, k) %>%
   mutate(prop = n/sum(n)) %>%
   # mutate(total = sum(n)) %>%
@@ -125,7 +136,7 @@ data_ar_arm_weight <- data_counts_arm_group %>%
   left_join(
     weights_arm_weight, by = c("arm", "outcome", "k", "age", "sex")
   ) %>%
-  group_by(subgroup, outcome, k) %>%
+  group_by(cohort, outcome, k) %>%
   summarise(
     events = sum(events),
     ar_crude = sum(events)/sum(n),
@@ -143,11 +154,11 @@ readr::write_csv(
   here::here("output", "tte", "tables", "ar_arm_weight.csv")
 )
 
-### age- and sex-standardised AR
+### 3. age- and sex-standardised AR within cohort
 # AR grouped by age, sex
 data_counts <- data_tte %>%
   left_join(data_all, by = "patient_id") %>% 
-  group_by(subgroup, outcome, k, age, sex) %>%
+  group_by(cohort, outcome, k, age, sex) %>%
   summarise(
     n = n(),
     events = sum(status),
@@ -159,7 +170,7 @@ data_counts <- data_tte %>%
 # within each outcome and comparison period, 
 # get proportion of general population in each sex:age_band
 arm_weights <- data_counts %>%
-  filter(subgroup == "noncancer") %>%
+  filter(cohort == "noncancer") %>%
   group_by(outcome, k) %>%
   mutate(total = sum(n)) %>%
   ungroup() %>%
@@ -172,7 +183,7 @@ data_ar <- data_counts %>%
   left_join(
     arm_weights, by = c("outcome", "k", "age", "sex")
   ) %>%
-  group_by(subgroup, outcome, k) %>%
+  group_by(cohort, outcome, k) %>%
   summarise(
     events = sum(events),
     ar_crude = sum(events)/sum(n),
