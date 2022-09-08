15f33a0c0348cf626eb4127352b3a4a3560f2d0d
Elsie Horne <31345323+elsie-h@users.noreply.github.com>
2022-09-08 19:29:23 +0100

    Update text_estimates.R

--- analysis/post_release/text_estimates.R
+++ analysis/post_release/text_estimates.R
@@ -8,25 +8,17 @@ release_folder <- here::here("release20220622")
 subgroups <- readr::read_rds(
   here::here("analysis", "lib", "subgroups.rds"))
 
-
-# eligibility_count_all <- readr::read_csv(
-#   here::here(release_folder, "eligibility_count_all.csv")
-# )
-# 
-# eligibility_count_all %>%
-#   mutate(across(c(n,n_removed), ~scales::comma(.x, accuracy = 1))) %>%
-#   print(n=Inf)
-# 
-# #TODO
-# # sort out boxes E and pink for both vax and unvax
-
 ################################################################################
+# Study population results section
+
 table1_main_TRUE_REDACTED <- readr::read_csv(
   "release20220622/table1_main_TRUE_REDACTED.csv"
   )
 cols <- names(table1_main_TRUE_REDACTED[,3:8])
 counts <- as.numeric(str_remove_all((unlist(table1_main_TRUE_REDACTED[1,3:8])), ","))
 names(counts) <- cols
+
+## number eligibile for each group
 # BNT162b2
 sum(counts[c(1,4)])
 # ChAdOx1
@@ -34,6 +26,7 @@ sum(counts[c(2,5)])
 # unvax
 sum(counts[c(3,6)])
 
+## percent of each group in the cancer cohort
 # BNT162b2
 100*counts[1]/sum(counts[c(1,4)])
 # ChAdOx1
@@ -41,6 +34,7 @@ sum(counts[c(3,6)])
 # unvax
 100*counts[3]/sum(counts[c(3,6)])
 
+## total in each cohort
 # cancer cohort:
 scales::comma(sum(counts[str_detect(names(counts), "_1")]))
 # general cohort
@@ -50,6 +44,8 @@ table1_age_TRUE_REDACTED <- readr::read_csv(
   "release20220622/table1_age_TRUE_REDACTED.csv"
 )
 
+# percent of those aged 18-69 years in JCVI groups 4b & 6 in each cohort
+# (5=cancer, 7=non-cancer)
 table1_age_TRUE_REDACTED %>% 
   filter(Variable=="JCVI group", Characteristic %in% c("04b", "06")) %>% 
   arrange(Characteristic) %>%
@@ -59,7 +55,6 @@ table1_age_TRUE_REDACTED %>%
                 )
          ) %>%
   summarise(across(everything(), sum))
-
 
 ################################################################################
 event_counts <- readr::read_csv(
@@ -137,26 +132,46 @@ event_counts %>%
 estimates_all <- readr::read_csv(
   here::here(release_folder, "estimates_all.csv")
 ) %>%
+  mutate(across(subgroup, factor, 
+                levels = seq_along(subgroups),
+                labels = subgroups)) %>%
+  filter(
+    variable == "k",
+    !reference_row,
+    model=="max_adjusted",
+    outcome %in% c("postest", "covidadmitted", "coviddeath", "noncoviddeath"),
+    comparison=="both",
+    prior
+  ) %>%
   mutate(across(c(estimate, conf.low, conf.high), exp)) 
+
+metareg_res <- readr::read_rds(here::here(release_folder, "metareg_res.rds")) %>%
+  right_join(
+    estimates_all %>%
+      distinct(subgroup, comparison, outcome, model, prior)
+  ) %>%
+  select(subgroup, comparison, outcome, model, prior, starts_with("logrhr")) %>%
+  mutate(across(starts_with("logrhr"), ~round(exp(.x),2)))
 
 estimates_all %>%
   filter(
-    variable == "k",
-    subgroup %in% 1:2,
-    !reference_row,
-    model=="unadjusted",
-    outcome %in% c("postest", "covidadmitted", "coviddeath"),
-    comparison=="both",
-    prior
+   period %in% c(1,6)
     ) %>%
-  group_by(subgroup, outcome) %>%
-  mutate(min_k = min(period), max_k = max(period)) %>%
-  ungroup() %>%
-  filter(period==min_k|period==max_k) %>%
   mutate(across(c(estimate, conf.low, conf.high), ~round(100*(1-.x),1)))  %>%
   transmute(
     subgroup, outcome, period, 
-    value = glue("{estimate}% ({conf.high}, {conf.low})")
+    value = glue("{estimate}% [{conf.high}, {conf.low}]")
+  ) %>%
+  pivot_wider(
+    names_from = subgroup,
+    values_from = value
+  )
+
+
+metareg_res %>%
+  transmute(
+    subgroup, outcome, 
+    value = glue("{logrhr} [{logrhr_lower}, {logrhr_higher}]")
   ) %>%
   pivot_wider(
     names_from = subgroup,
@@ -167,26 +182,27 @@ estimates_all %>%
 # percentage point differences in 3-6 o 23-26
 ppoint <- estimates_all %>%
   filter(
-    variable == "k",
-    subgroup %in% 3:4,
-    !reference_row,
-    model=="unadjusted",
-    outcome %in% c("postest", "covidadmitted", "coviddeath"),
-    comparison=="both",
-    prior
+    as.integer(subgroup) %in% 1:2
     ) %>%
   mutate(across(c(estimate, conf.low, conf.high), ~round(100*(1-.x),1)))  
 
 ppoint %>%
-  select(subgroup, outcome, period, estimate) %>%
+  mutate(text = glue("{format(estimate,nsmall=1,trim=TRUE)}% [{format(conf.high,nsmall=1,trim=TRUE)}, {format(conf.low,nsmall=1,trim=TRUE)}]")) %>%
+  select(subgroup, outcome, period, estimate, text) %>%
   pivot_wider(
     names_from = period,
-    values_from = estimate
+    values_from = c(estimate, text)
   ) %>%
   mutate(
-    `1` - `6`,
-    `2` - `6`
-    )
+    diff_1_6 = estimate_1 - estimate_6,
+    diff_2_6 = estimate_2 - estimate_6
+  ) %>%
+  transmute(
+    outcome,
+    text_1_6 = glue("{format(diff_1_6,nsmall=1)} percentage points in our {subgroup} cohort ({text_1} during 3-6 weeks versus {text_6} during 23-26 weeks)"),
+    text_2_6 = glue("{format(diff_2_6,nsmall=1)} percentage points in our {subgroup} cohort {text_2} during 3-6 weeks versus {text_6} during 23-26 weeks")
+    ) %>%
+  select(outcome, contains("1"))
 
 
 ###############################################################################
